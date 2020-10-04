89506cbb7278954f25dc2fda332f1752fa17a6fb
HPE Admin <hpeadmin@hercules.dlg.dubai>
2020-10-04 10:23:07 +0400

    Add ip forwarding for gateway

--- scripts/kvm_ipforwarding.sh
+++ scripts/kvm_ipforwarding.sh
@@ -18,6 +18,7 @@ if [[ "$2" == "on" ]]; then
     sudo /sbin/iptables -t nat -I PREROUTING -p tcp -d $KVM_HOST_IP --dport 80 -j DNAT --to-destination ${DEST_IP}:80
     sudo /sbin/iptables -t nat -I PREROUTING -p tcp -d $KVM_HOST_IP --dport 8080 -j DNAT --to-destination ${DEST_IP}:8080
     sudo /sbin/iptables -t nat -I PREROUTING -p tcp -d $KVM_HOST_IP --dport 443 -j DNAT --to-destination ${DEST_IP}:443
+    sudo /sbin/iptables -t nat -I PREROUTING -p tcp -d $KVM_HOST_IP --dport 10000:11000 -j DNAT --to-destination ${DEST_IP}
     sudo /sbin/iptables -I FORWARD -m state -d ${DEST_IP}/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
     echo "Port forwarding for ${DEST_IP} on port 80/8080/443 enabled"
 else
@@ -26,6 +27,7 @@ else
     sudo /sbin/iptables -t nat -D PREROUTING -p tcp -d $KVM_HOST_IP --dport 80 -j DNAT --to-destination ${DEST_IP}:80
     sudo /sbin/iptables -t nat -D PREROUTING -p tcp -d $KVM_HOST_IP --dport 8080 -j DNAT --to-destination ${DEST_IP}:8080
     sudo /sbin/iptables -t nat -D PREROUTING -p tcp -d $KVM_HOST_IP --dport 443 -j DNAT --to-destination ${DEST_IP}:443
+    sudo /sbin/iptables -t nat -D PREROUTING -p tcp -d $KVM_HOST_IP --dport 10000:11000 -j DNAT --to-destination ${DEST_IP}
     sudo /sbin/iptables -D FORWARD -m state -d ${DEST_IP}/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
     echo "Port forwarding for ${DEST_IP} on port 80/8080/443 disabled"
     set -e
